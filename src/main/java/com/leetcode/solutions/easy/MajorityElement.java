2da5e5dd08230a9f2c5b80f6e908ac6ee62d49c8
Majid Hameed <majid.hameed@gmail.com>
2022-01-15 14:37:17 +0800

    MajorityElement solved.

---
+++ src/main/java/com/leetcode/solutions/easy/MajorityElement.java
@@ -0,0 +1,63 @@
+package com.leetcode.solutions.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * <a href="https://leetcode.com/problems/majority-element/">169. Majority Element</a>
+ * <br/>
+ * Given an array nums of size n, return the majority element.
+ * <br/> <br/>
+ * The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+ * <pre>
+ * Constraints:
+ *
+ * n == nums.length
+ * 1 <= n <= 5 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * </pre>
+ * <br/>
+ * Follow-up: Could you solve the problem in linear time and in O(1) space?
+ */
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        final Map<Integer, Integer> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        int target = nums[0];
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+                if (map.get(num) > nums.length / 2) {
+                    return num;
+                }
+            } else {
+                map.put(num, 1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (max < entry.getValue()) {
+                target = entry.getKey();
+                max = entry.getValue();
+            }
+        }
+
+        return target;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(3, majorityElement(new int[]{3, 2, 3}));
+
+        assertEquals(2, majorityElement(new int[]{2, 2, 1, 1, 1, 2, 2}));
+    }
+}
